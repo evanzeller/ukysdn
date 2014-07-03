0d7448a920346ccf1ade92872f1d1d285fb6b17f
Evan Zeller <evanrzeller@gmail.com>
2014-07-03 03:45:45 -0400

    Added Dijkstra implementation
    Signed-off-by: Evan Zeller <evanrzeller@gmail.com>

--- PathService.py
+++ PathService.py
@@ -1,6 +1,17 @@
+import heapq
+
+class PQEntry:
+    def __init__(self, value, pathservice):
+        self.value = value
+        self.pathservice = pathservice
+
+    def __cmp__(self, other):
+        return cmp(self.pathservice.dist[self.value], self.pathservice.dist[other.value])
+
 class PathService:
     def __init__(self):
         self.path = []
+        self.dist = []
     
     def setIOService(self, io):
         self.io = io
@@ -9,4 +20,39 @@ class PathService:
         self.inv = inv
 
     def dijkstra(self, src, dst):
+        self.dist = []
+        previous = []
+        weight = []
+        nodes = inv.getNodes()
+        edges = inv.getEdges()
+        for node in nodes:
+            weight[nodes.index(node)] = []
         
+        queue = []
+        
+        for edge in edges:
+            weight[edge["source"]][edge["target"]] = 1
+            weight[edge["target"]][edge["source"]] = 1
+
+        self.dist[src] = 0
+        for node in nodes:
+            if nodes.index(node) != src:
+                self.dist[nodes.index(node)] = float('inf')
+                previous[nodes.index(node)] = None
+
+            heapq.heappush(queue, PQEntry(nodes.index(node), self))
+        
+        while queue:
+            u = heapq.heappop(queue)
+            for node in u["neighbours"]
+                alt = self.dist[u] + weight[u][node]
+                if alt < dist[node]:
+                    dist[node] = alt
+                    previous[node] = u
+                    heapq.heapify(queue)
+
+        print previous        
+        return previous
+        
+        
+                
--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import IOService
 if __name__ == "__main__":
     inv = InventoryService.InventoryService()
     io = IOService.IOService()
-    io.setController("172.24.240.19", "8080", "admin", "cc-nie")
+    io.setController("127.0.0.1", "8080", "admin", "admin")
     inv.setIOService(io)
     inv.getNodesFromController()       
     inv.getEdgesFromController()
-    inv.initAll() 
+    inv.initAll()
+    print inv.getEdges() 
